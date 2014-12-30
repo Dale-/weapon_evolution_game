f61e2e0c84c5b50b6ef6492ab15050720d2d1b72
dale <18717394542@163.com>
2014-12-30 20:55:32 +0800

    Coding common-people-test and test pass

---
+++ scripts/__tests__/model/common-people-test.js
@@ -0,0 +1,19 @@
+jest.dontMock('../../model/common-people');
+jest.dontMock('../../model/player');
+
+
+describe('CommonPeople', function() {
+
+  describe('.attackPoint', function() {
+
+    it('it should be return correct hp', function() {
+
+      var CommonPeople = require('../../model/common-people');
+      var commonPeople = new CommonPeople('Damon', 5, 1);
+      var result = commonPeople.attackPoint(
+                   {name: 'Stefan', hp: 3, attackValue:1});
+
+      expect(result).toBe(2);
+    });
+  });
+});
