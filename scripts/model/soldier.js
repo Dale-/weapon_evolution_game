21316e51d5e7dd7a643e2329f19b0beab32044b2
dale <18717394542@163.com>
2014-12-30 21:40:04 +0800

    add getAttackValue() in soldier.js

--- scripts/model/soldier.js
+++ scripts/model/soldier.js
@@ -1,12 +1,19 @@
 var Player = require('./player');
+var _ = require('lodash');
 
 function Soldier(name, hp, attackValue, weapon) {
   Player.call(this, name, hp, attackValue);
   this.weapon = weapon;
-  // this.attackValue
 }
 
 Soldier.prototype = Object.create(Player.prototype);
 Soldier.prototype.constructor = Soldier;
+
+Soldier.prototype.getAttackValue = function() {
+  var myWeapon = _.find(Weapon.all(),function(weapon) {
+    return weapon.name === this.weapon;
+  });
+  this.attackValue += myWeapon.attackValue;
+};
 
 module.exports = Soldier;
