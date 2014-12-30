d579e2213eb70b778b39dfa66d6da5b814baa9f2
dale <18717394542@163.com>
2014-12-30 21:05:17 +0800

    Coding soldier-test completely

---
+++ scripts/__tests__/model/soldier-test.js
@@ -0,0 +1,19 @@
+jest.dontMock('../../model/soldier');
+jest.dontMock('../../model/player');
+
+
+describe('Soldier', function() {
+
+  describe('.attackPoint', function() {
+
+    it('it should be return correct hp', function() {
+
+      var Soldier = require('../../model/soldier');
+      var soldier = new Soldier('Damon', 5, 1);
+      var result = soldier.attackPoint(
+                   {name: 'Stefan', hp: 3, attackValue:1});
+
+      expect(result).toBe(2);
+    });
+  });
+});
