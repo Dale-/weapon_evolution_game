b9b4308d33d8b2be39a8acb07f2bac5bc8f4f6bc
dale <18717394542@163.com>
2014-12-30 20:50:41 +0800

    Coding commonPeople in commonPeople.js

---
+++ scripts/model/commonPeople.js
@@ -0,0 +1,11 @@
+var Player = require('./player');
+
+function CommonPeople(name, hp, attackValue) {
+  Player.call(this, name, hp, attackValue);
+}
+
+CommonPeople.prototype = Object.create(Player.prototype);
+
+CommonPeople.prototype.constructor = CommonPeople;
+
+module.exports = CommonPeople;
