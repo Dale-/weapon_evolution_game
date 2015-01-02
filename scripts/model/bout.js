5c923c8c01600a3c3f5c6716150d7dac01c4f85e
dale <18717394542@163.com>
2015-01-02 18:37:32 +0800

    Coding boutDizzy() in bout.js

--- scripts/model/bout.js
+++ scripts/model/bout.js
@@ -2,7 +2,7 @@ function Bout(player, soldier) {
   this.player = player;
   this.soldier = soldier;
   this.times = 1;
-  this.dizzyTimes = 1;
+  this.dizzyTimes = 0;
 }
 
 Bout.prototype.boutBlood = function() {
@@ -64,9 +64,14 @@ Bout.prototype.boutCtriticalStrike = function() {
             value + '点伤害' + '，' + this.getPlayerName() + '剩余生命：' +
             this.getPlayerHP() + '\n';
 
+    this.dizzyTimes ++;
+
   } else {
+
     info += '//' + this.getSoldierName() + '进攻\t' +
             this.getPlayerName() + '剩余生命：' + this.getPlayerHP() + '\n';
+
+    this.dizzyTimes = this.dizzyTimes > 0 ? this.dizzyTimes-- : this.dizzyTimes;
   }
 
   if(this.getPlayerHP() <= 0) {
@@ -74,9 +79,15 @@ Bout.prototype.boutCtriticalStrike = function() {
     return info;
   }
 
-  this.soldier.hp -= this.getPlayerAP();
+  if(this.dizzyTimes > 0) {
 
-  info += '//' + this.getPlayerName() + '进攻\n';
+    info += this.getPlayerName() +'晕倒了，无法攻击, 眩晕还剩：' +
+            this.dizzyTimes + '轮';
+  } else {
+
+    this.soldier.hp -= this.getPlayerAP();
+    info += '//' + this.getPlayerName() + '进攻\n';
+  }
 
   if(this.getSoldierHP() <= 0) {
     info += this.getSoldierName() + '被打死了';
@@ -117,6 +128,51 @@ Bout.prototype.boutFrozen = function() {
   }else if(this.times%3 === 0) {
     info += this.getPlayerName() + '冻得直哆嗦，没有击中' +
             this.getSoldierName() + '\n';
+  } else {
+    this.soldier.hp -= this.getPlayerAP();
+    info += '//' + this.getPlayerName() + '进攻\n';
+  }
+
+  if(this.getSoldierHP() <= 0) {
+    info += this.getSoldierName() + '被打死了';
+    return info;
+  }
+  this.times ++;
+  return info;
+};
+
+Bout.prototype.boutDizzy = function() {
+  var info = '';
+
+  var value = (this.getSoldierAP() + this.getSoldierWeaponAp());
+  this.player.hp -= value;
+
+  if(this.times === 1 || this.times === 2) {
+
+    info += '战士' + this.getSoldierName() + '用' +
+            this.getSoldierWeaponName() + '攻击了普通人' +
+            this.getPlayerName() + '，' + this.getPlayerName() +
+            '受到了' + value + '点伤害，' + this.getPlayerName() +
+            this.getSoldierWeaponSkillInfo() + this.getPlayerName() +
+            '剩余生命：' + this.getPlayerHP() + '\n';
+
+  } else {
+    info += '//' + this.getSoldierName() + '进攻\n';
+  }
+
+  if(this.getPlayerHP() <= 0) {
+    info += this.getPlayerName() + '被打死了';
+    return info;
+  }
+
+  if(this.times%3 === 1) {
+    info += this.getPlayerName() + this.getSoldierWeaponSkillInfo() +
+    this.getPlayerName() + '剩余生命：' + this.getPlayerHP() + '\n' +
+    '//' + this.getPlayerName() +'进攻\n';
+
+  }else if(this.times%3 === 0) {
+    info += this.getPlayerName() + '冻得直哆嗦，没有击中' +
+    this.getSoldierName() + '\n';
   } else {
     this.soldier.hp -= this.getPlayerAP();
     info += '//' + this.getPlayerName() + '进攻\n';
