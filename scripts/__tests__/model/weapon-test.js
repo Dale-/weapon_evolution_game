49dbf1ff7626e3d56f965f8e6fd5e7b72ac54ebe
dale <18717394542@163.com>
2015-01-02 16:18:42 +0800

    rewrite weapon-test.js and test pass

--- scripts/__tests__/model/weapon-test.js
+++ scripts/__tests__/model/weapon-test.js
@@ -1,29 +1,20 @@
-// jest.dontMock('../../model/weapon.js');
-//
-//
-// describe('Weapon', function() {
-//
-//   describe('.all', function() {
-//
-//     it('it should be return correct Array', function() {
-//
-//       var Weapon = require('../../model/weapon');
-//       var result = Weapon.all();
-//
-//       expect(result[0].name).toBe('利剑');
-//     });
-//   });
-// 
-//   describe('#getAttackValue', function() {
-//
-//     it('it should be return correct attackValue', function() {
-//
-//       var Weapon = require('../../model/weapon');
-//       var weapon = new Weapon('利剑', 1, ['致命一击'], 1);
-//       var result = weapon.getAttackValue();
-//
-//       expect(result).toBe(6);
-//     });
-//   });
-//
-// });
+jest.dontMock('../../model/weapon.js');
+jest.dontMock('../../model/skill.js');
+
+
+describe('Weapon', function() {
+
+  describe('.all', function() {
+
+    it('it should be return correct Array', function() {
+
+      var Weapon = require('../../model/weapon');
+      var Skill = require('../../model/skill');
+      var result = Weapon.all();
+
+      expect(result[0].name).toBe('利剑');
+      expect(result[0].skill.name).toBe('致命一击');
+    });
+  });
+
+});
