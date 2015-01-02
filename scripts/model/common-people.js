1f9213125080ea3574215590b6d521673e9567e0
dale <18717394542@163.com>
2015-01-02 22:06:14 +0800

    modify common-people.js of info

--- scripts/model/common-people.js
+++ scripts/model/common-people.js
@@ -8,23 +8,9 @@ CommonPeople.prototype = Object.create(Player.prototype);
 CommonPeople.prototype.constructor = CommonPeople;
 
 CommonPeople.prototype.attack = function(soldier) {
+  
   soldier.hp -= this.attackPoint;
-
-  var info = this.name + '攻击了' + soldier.name + ' , ' +
-             soldier.name  + '受到了' +
-             (this.attackPoint - soldier.defenseValue) + '点伤害 ' +
-             soldier.name +'剩余生命值：' + soldier.hp + '\n\n';
-
-  return info;
-};
-
-CommonPeople.prototype.attackText = function(soldier) {
-  soldier.hp -= this.attackValue;
-
-  var info = this.name + '攻击了' + soldier.name + ' , ' +
-  soldier.name  + '受到了' +
-  (this.attackValue - soldier.defenseValue) + '点伤害 ' +
-  soldier.name +'剩余生命值：' + soldier.hp + '\n\n';
+  var info = '//' + this.name + '进攻';
 
   return info;
 };
